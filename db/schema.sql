5e5194e9668c531e252203d618d0a56db88c63ac
Lawson Kelly <wlawsonkelly@gmail.com>
2020-12-20 17:44:28 -0500

    updating schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE departments (
+	id INTEGER,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+	id INTEGER,
+    title VARCHAR(30),
+    salray DECIMAL,
+    department_id INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+	id INTEGER,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+);
